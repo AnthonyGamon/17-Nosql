c515ed0686b8e66855d7c500ce412388a9696702
Anthony Gamon <anthonygamon@Anthonys-MBP-2.attlocal.net>
2021-01-20 19:41:10 -0600

    working on models

--- modals/workouts.js
+++ modals/workouts.js
@@ -1,3 +1,39 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    exercise: [
+        {
+            type: {
+                type: String, 
+                trim: true,
+                required: "Enter an exercise"
+            },
+            name: {
+                type: String, 
+                trim: true,
+                required: "Enter an exercise name"
+            },
+            duration: {
+                type: Number, 
+            },
+            reps: {
+                type: Number, 
+            },
+            weight: {
+                type: Number, 
+            },
+            sets: {
+                type: Number, 
+            },
+            distance: {
+                type: Number, 
+            },
+        }
+    ]
+  });
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
